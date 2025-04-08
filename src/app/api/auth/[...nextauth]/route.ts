ccdf95c2707317e89d49d2c07242cedad93a86e2
Jason Botterill <jasonbotterill@hotmail.com>
2025-04-08 19:02:19 +0800

    Add fix type check on export issue with auth options

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,20 +1,6 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import type { AuthOptions } from "next-auth";
-
-export const authOptions: AuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET, // Recommended explicitly
-  // Optional custom page configuration:
-  // pages: {
-  //   signIn: '/auth/signin',
-  // },
-};
+import { authOptions } from "@/lib/authOptions";
 
 const handler = NextAuth(authOptions);
+
 export { handler as GET, handler as POST };
